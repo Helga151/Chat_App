52b5ab9701e8228f1cbb61e71a02d3abf555e46e
Unun0kwaD <pawel.mar.2001@gmail.com>
2022-01-15 14:58:11 +0100

    dotestowania

--- client.c
+++ client.c
@@ -134,7 +134,11 @@ int Register() {
 void HeartBeat(int queue){
     int t=3;
     while (t){
-        if(msgrcv(queue,&mes,(sizeof(mes) - sizeof(long)),11,IPC_NOWAIT)>0){
+        int received=0;
+        while (msgrcv(queue,&mes,(sizeof(mes) - sizeof(long)),11,IPC_NOWAIT)>0){
+            received=1;
+        }
+        if(received){
             mes.mtype=12;
             //printf("%s\n",mes.mtext);
             //strcpy(mes.mtext,"Beat");
